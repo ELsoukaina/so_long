8f70e5d3a3ee1f00ac82e9643d9c69e2b043fd01
Soukaina El jalaoui <sel-jala@e3r5p11.1337.ma>
2022-06-26 00:20:23 +0100

    so_long_v01

---
+++ srcs/ft_check_map_is_valid.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_map_is_valid.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmoussam <nmoussam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/17 07:13:51 by nmoussam          #+#    #+#             */
+/*   Updated: 2022/04/17 07:18:25 by nmoussam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+void    ft_check_map_is_valid(map_s **map)
+{
+    // check if this maps is rectangulaire && suraround by walls and check element
+}
